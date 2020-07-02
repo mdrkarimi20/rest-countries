3927707cd9145eeeb722f2e1924d102b65b1e53d
mdrkarimi20 <mdrkarimi20@gmail.com>
2020-07-02 18:32:28 +0430

    show map contries

--- app.js
+++ app.js
@@ -25,17 +25,36 @@ $(document).ready(function() {
         apiKey: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjVjMDA0MzY4ZmE2YjdjYzNlNzM5ZDc2N2MxOWUwM2QxMWQwZDYyOGY1MDYxMGVlNTEwOTMyOTQ2ZGNmMzZmZDQxYjVlNjViYTJlYmQ3ZTM3In0.eyJhdWQiOiI5NTI2IiwianRpIjoiNWMwMDQzNjhmYTZiN2NjM2U3MzlkNzY3YzE5ZTAzZDExZDBkNjI4ZjUwNjEwZWU1MTA5MzI5NDZkY2YzNmZkNDFiNWU2NWJhMmViZDdlMzciLCJpYXQiOjE1OTE0NDI5NDIsIm5iZiI6MTU5MTQ0Mjk0MiwiZXhwIjoxNTk0MDM0OTQyLCJzdWIiOiIiLCJzY29wZXMiOlsiYmFzaWMiXX0.cOua4iT8wU9z4jV_UyqCRUGSlQerscT7JC2O8J6yrlpsi6HCE1jh9IsyI53S8GMixa713Wh0r2VDXljlsO-dM8-BOKXCLr-tIsPUznOgar1sBnGWYunDxJzknKJl1oEPv75Ugu4j-j7W1QNoioVHGYrQLvbh_49mGTlq8EZzweEZ9D6Z0Ct-FzHMqKXDy6SY0XQc3aI_UV4K-y6lKh4OeB7rfap-5CLbsJs_4XQlRV3cBeqlqV_R87zrDk7AgDIJl2klzKR4JSBzbjHVqHE8-qrFovMgqAM4lZMTWL8c4YLklDpHk8GTzpdSg6KSs3wOxOnK-8f9qY2V-1O29fswEA",
     });
     app.addLayers();
-    app.addMarker({
-        name: "basic-marker",
-        latlng: {
-            lat: res.latlng[0],
-            lng: res.latlng[1],
-        },
-        popup: {
-            title: {
-                html: res.translations.fa,
-            },
-            open: true,
-        },
+
+    $("#select-country").change(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: `https://restcountries.eu/rest/v2/alpha/${$(this).val()}`,
+            method: "GET",
+        }).done(function(res) {
+            $("#information-country").text(res.name);
+            $("#native-name").text(res.nativeName);
+            $("#capital").text(res.capital);
+            $("#region").text(res.region);
+            $("#population").text(res.population);
+            $("#languages").text(res.languages[0].name);
+            $("#time-zones").text(res.timezones);
+            $(".calling-code-value").text(res.callingCodes);
+            $("#flag-image").attr("src", res.flag);
+
+            app.addMarker({
+                name: "basic-marker",
+                latlng: {
+                    lat: res.latlng[0],
+                    lng: res.latlng[1],
+                },
+                popup: {
+                    title: {
+                        html: res.translations.fa,
+                    },
+                    open: true,
+                },
+            });
+        });
     });
 });
